071277b3dd6922c0af427065c7d35ec37f226b1f
kashyappriyanshu0123 <93027668+kashyappriyanshu0123@users.noreply.github.com>
2024-04-15 11:45:55 +0000

    container config done

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,14 @@
+const accountid = 12344
+let accountemail = "priyanshukashyap024@gmail.com"
+var accountpassword = "123455"
+accountcity = "jaipur"
+
+let accountstate;
+accountemail = "kashayp@12.com"
+accountpassword = "123466"
+accountcity = "bengalluru"
+
+
+console.log(accountemail);
+
+console.table([accountid,accountemail,accountpassword,accountcity,accountstate]);
