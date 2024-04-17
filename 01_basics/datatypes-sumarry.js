b545a122bb233037a05320a111b190e3b3f5bb88
kashyappriyanshu0123 <93027668+kashyappriyanshu0123@users.noreply.github.com>
2024-04-17 13:38:12 +0000

    change in location of file

---
+++ 01_basics/datatypes-sumarry.js
@@ -0,0 +1,38 @@
+// Primitive data types
+
+// 7 types: : String , Number , Boolean ,null ,undefined,symbol,big int
+
+
+// Q -> is js a dynamically type lang or statically type language
+
+//in js it's not important to defined the type of the variable
+const outsidetemp = null;
+let usermail = undefined;
+
+console.log(usermail);
+
+//Symbol 
+
+const id = Symbol("123")
+const anotherid = Symbol("123") //  the both values are passed here is same string , nut the result will be different for both of them
+
+console.log(id==anotherid);
+
+//bigint
+const bignumber = 234567890123n
+console.log(typeof bignumber);
+
+//console.log(typeof outsidetemp); // output object
+
+//referencen(non - prmitive datatypes)
+
+//Array , object , functions -> these non-primitive data type return type are object
+const myfunction = function()
+{
+    console.log("helloworld");
+}
+console.log(typeof myfunction); // output function but in actually it calls as obj function
+
+const heros = ["shaktiman","krrish","flying jat" ];
+
+console.log(typeof heros);// output object
