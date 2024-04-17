94f02a02925073992944d772a726e287d4fb3673
kashyappriyanshu0123 <93027668+kashyappriyanshu0123@users.noreply.github.com>
2024-04-17 12:41:37 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,10 @@
+// console.log(2 > 1);
+// console.log("2" > 1); //automatically js covert "2" string  in a number
+
+console.log(null == 0);
+console.log(null >= 0);  //it's true
+console.log(null > 0);
+
+// === -> (strick check) -> the value will be checked strictly means the datatype will also be checked
+
+console.log("2" === 2); //it will be checked strictly 
