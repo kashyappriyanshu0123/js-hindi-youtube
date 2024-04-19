1e182645cb52c1e9b313230df20f605922532c6e
kashyappriyanshu0123 <93027668+kashyappriyanshu0123@users.noreply.github.com>
2024-04-19 20:12:42 +0000

    array in depth

--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -21,3 +21,9 @@ console.log(real_another_array);
 console.log(Array.isArray('hitesh'));
 console.log(Array.from("hitess")); // it will covert it into array bcoz of from
 console.log(Array.from({name:"hitesh"}));
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1, score2, score3));
