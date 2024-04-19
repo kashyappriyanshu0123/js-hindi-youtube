7cd2a35741699b2d8329daf1762581cfd222f333
kashyappriyanshu0123 <93027668+kashyappriyanshu0123@users.noreply.github.com>
2024-04-19 20:11:32 +0000

    array in depth

---
+++ 02_basics/02_array.js
@@ -0,0 +1,23 @@
+const marvel = ["thor" , "ironman" , "spiderman"]
+const dc_heros = ["superman","batman","flash"] // it's the 4th element if we are pushing it in marvel array
+
+//marvel.push(dc_heros);
+//console.log(marvel);
+
+const allheros = marvel.concat(dc_heros)
+console.log(allheros);
+
+
+const all_new_heros = [...marvel, ...dc_heros] //spread 
+
+console.log(all_new_heros);
+
+const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
+
+const real_another_array = another_array.flat(Infinity)
+console.log(real_another_array);
+
+
+console.log(Array.isArray('hitesh'));
+console.log(Array.from("hitess")); // it will covert it into array bcoz of from
+console.log(Array.from({name:"hitesh"}));
