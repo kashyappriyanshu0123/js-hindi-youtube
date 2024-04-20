4d4a252ce20c1724a1c0d4557996c80031b236c9
kashyappriyanshu0123 <93027668+kashyappriyanshu0123@users.noreply.github.com>
2024-04-20 09:54:55 +0000

    object

---
+++ 02_basics/03_object.js
@@ -0,0 +1,38 @@
+
+//object literals
+
+//symbol
+
+const mySym = Symbol("key1") // declaring symbol
+
+const jsuser = {
+    name : "priyanshu",
+    "full name" : "priyanshu kashyap" ,// there is no way i can access it through (.) i have to access through brackets only
+    [mySym]: "mykey1", //we have to put the key in bracket for refering the symbol -> if it's not in bracket then its string
+    age : 20,
+    location : "patna",
+    email : "priyanshukashyap024@gmail.com",
+    isloggedin : "false",
+    lastlogindays : ["Monday" , "Tuesday"]
+
+}
+// console.log(jsuser.email);
+// console.log(jsuser["email"]); //we have to pass the email as a string because bts it's placed like a string 
+// console.log(jsuser["full name"]);
+// console.log(typeof jsuser.mySym);
+// console.log(jsuser[mySym]);
+
+jsuser.email = "priyanshuchatgpt@.com"
+// Object.freeze(jsuser)
+jsuser.email = "priyanshumicrosft@.com"
+// console.log(jsuser);
+
+jsuser.greeting = function(){ //greeting is a method
+    console.log("hello js world");
+}
+console.log(jsuser.greeting());
+jsuser.greetingtwo = function(){ //greeting is a method
+    console.log(`hello js user, ${this.name}`);
+}
+console.log(jsuser.greeting());
+console.log(jsuser.greetingtwo());
