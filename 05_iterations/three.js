629b76fef560a98a64dd3fa89af040444de7ece3
kashyappriyanshu0123 <93027668+kashyappriyanshu0123@users.noreply.github.com>
2024-04-24 12:24:39 +0000

    three

---
+++ 05_iterations/three.js
@@ -0,0 +1,51 @@
+// for of loop : (it's a array specific loop)
+
+
+// const arr = [1, 2, 3, 4, 5]
+
+// for (const num of arr) {
+//     console.log(num);
+// }
+
+const greetings1 = ["hello world!"]
+
+for (const val of greetings1) {
+    
+    console.log(`each char is ${val}`);
+    
+}
+
+// remove the space
+
+
+// const greetings = ["hello world!"];
+// let i = 0;
+// for (const val of greetings) {
+//     for (const char of val) {
+//         if (char === ' ') {
+//             continue; // Skip this iteration if the character is a space
+//         }
+        
+//         console.log(`each char is ${char}`);
+//     }
+// }
+
+// const greetings = ["hello world!"];
+
+// // Iterate over each character in the string
+// for (const char of greetings[0]) {
+//     // Skip the iteration if the character is a space
+//     if (char === ' ') {
+//         continue;
+//     }
+//     // Log each non-space character
+//     console.log(`each char is ${char}`);
+// }
+
+
+//Maps -> Maps is object in itself in js everything is object
+
+const map = new Map()
+
+
+
