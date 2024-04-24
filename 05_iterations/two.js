adf998308f10b3f73bca32515117569b463e666c
kashyappriyanshu0123 <93027668+kashyappriyanshu0123@users.noreply.github.com>
2024-04-24 12:24:30 +0000

    two

---
+++ 05_iterations/two.js
@@ -0,0 +1,27 @@
+// let index = 0
+
+// while (index <=10) {
+//     console.log(`value of index is ${index}`);
+//     index += 2;
+// }
+
+
+// let my = ['flash','superman','batman']
+// let ind = 0
+// while (ind < 3) {
+//     console.log(`my superheroes are : ${my[ind]}`);
+//     ind++
+// }
+
+
+
+//do - while
+
+
+
+
+let score = 1
+do {
+    console.log(`score is ${score}`);
+    score++
+} while (score <= 10);
