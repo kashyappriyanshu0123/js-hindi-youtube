9dfbac73e58c6d7767e0d79b4ec56ab7c50c2199
kashyappriyanshu0123 <93027668+kashyappriyanshu0123@users.noreply.github.com>
2024-05-02 00:45:19 +0000

    promise

---
+++ advance_one/promise.js
@@ -0,0 +1,17 @@
+//Promise creation
+
+const promiseone = new Promise(function(resolve,reject){
+   //do async task
+   //DB calls,cryptography,network
+   setTimeout(() => {
+    console.log('async task is completed') 
+   }, 1000);
+   
+})
+
+//promise consumption
+//.then have connection to reolve
+
+promiseone.then(function(){
+    console.log("promise is consumed");
+})
