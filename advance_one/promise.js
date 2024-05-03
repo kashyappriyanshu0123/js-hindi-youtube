372d873564df9349d3dd1f28ac1d66981b33cae0
kashyappriyanshu0123 <93027668+kashyappriyanshu0123@users.noreply.github.com>
2024-05-03 10:32:48 +0000

    promises

--- advance_one/promise.js
+++ advance_one/promise.js
@@ -1,17 +1,63 @@
-//Promise creation
+// //  Promise creation
+// const promiseOne = new Promise(function(myresolve, myreject){
+//     //Do an async task
+//     // DB calls, cryptography, network
+//     setTimeout(function(){
+//         myresolve()//for connecting myresolve and (.then) we have to call resolve method 
+//         console.log('Async task is compelete'); //it will execute first
+       
+//     }, 1000)
+// })
+// //"Consuming code"
+// promiseOne.then(function(){ //(.then direct connection with myresolve)
+//     console.log("Promise consumed");
+// })
 
-const promiseone = new Promise(function(resolve,reject){
-   //do async task like :
-   //DB calls,cryptography,network
-   setTimeout(() => {
-    console.log('async task is completed') 
-   }, 1000);
-   
+// new Promise( (resolve,reject) =>
+// {
+//     setTimeout(() => {
+//         console.log("async task 2");
+//         resolve()
+//     }, 1000);
+//     // setTimeout(function(){
+//     //     console.log("async task 2");
+//     //     resolve()
+//     // })
+// })
+// .then(function(){ //.then direct connection wth resolve
+//     console.log("async 2 resolved");
+// })
+
+// const promisethree = new Promise(function(resolve,reject){
+// setTimeout(function(){
+//   resolve({username : "Priyanshu kumar" , email : "kashyap@123.com" })
+//   },1000)
+// })
+
+
+
+// promisethree.then(function(user){
+//     console.log(user);
+// })
+
+
+const promiseFour = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        let error = false
+        if (!error) {
+            resolve({username: "hitesh", password: "123"})
+        } else {
+            reject('ERROR: Something went wrong')
+        }
+    }, 1000)
 })
 
-//promise consumption
-//.then have connection to reolve
-
-promiseone.then(function(){
-    console.log("promise is consumed");
+ promiseFour
+ .then((user) => {
+    console.log(user);
+    return user.username
+}).then((username) => {
+    console.log(username);
+}).catch(function(error){
+    console.log(error);
 })
