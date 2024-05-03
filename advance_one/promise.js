66e62a7081a91ead1e076cab7d9aa208d3fea76e
kashyappriyanshu0123 <93027668+kashyappriyanshu0123@users.noreply.github.com>
2024-05-03 13:18:28 +0000

    promises

--- advance_one/promise.js
+++ advance_one/promise.js
@@ -61,3 +61,54 @@ const promiseFour = new Promise(function(resolve, reject){
 }).catch(function(error){
     console.log(error);
 })
+.finally(() => {
+    console.log("the promise is either resolved or rejected"); //it will get printed if the prmise is resolve or rejected
+})
+
+const promisefive = new Promise(function(resolve,reject){
+    setTimeout(function(){
+       let error = true
+       if(!error){
+        resolve({username: "javascript" , password : "123"})
+       }
+       else{
+        reject("errror : js went wrong")
+       }
+    },1000)
+})
+
+async function consumefive(){
+try {
+    const response = await promisefive
+    console.log(response);
+    
+} catch (error) {
+    console.log(error);
+}
+} 
+consumefive()
+
+//both are same async await and .then
+
+// async function getallusers(){
+//     try {
+//         const response = await fetch ('https://jsonplaceholder.typicode.com/users')
+//         const data = await response.json()
+//         console.log(data);
+//     } catch (error) {
+//         console.log("E:",error);
+//     }
+// }
+// getallusers()
+
+fetch('https://api.github.com/users/hiteshchoudhary')
+.then((response) => {
+    return response.json()
+})
+.then((data) => {
+    console.log(data); //response.json return will handle by this (.then)
+})
+.catch((error) => console.log(error))
+
+// promise.all
+// yes this is also available, kuch reading aap b kro.
