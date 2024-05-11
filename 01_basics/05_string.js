fba1784285e3056c6d31be219acd8704ec76d888
kashyappriyanshu0123 <93027668+kashyappriyanshu0123@users.noreply.github.com>
2024-05-11 09:19:09 +0000

    strings

--- 01_basics/05_string.js
+++ 01_basics/05_string.js
@@ -4,6 +4,7 @@ const repcount = 50
 console.log(name + repcount);
 console.log(`hello my name is ${name.toUpperCase()} and my repo count is ${repcount}`); //we can also add method here as we added toupper
 
+console.log(`hey my name is ${name.toUpperCase} and my reocount is : ${repcount}`);
 const gamename = new String("hitesh-hc")
 
 console.log(gamename[0]); //for accessing the 0th key
@@ -15,15 +16,26 @@ console.log(gamename.indexOf('i'));
 const newstring = gamename.substring(0,4)
 console.log(newstring);
 
-const anotherstring = gamename.slice(-8,4) //we can use negative value in slice
+const anotherstring = gamename.slice(-8,7) //we can use negative value in slice
 
 console.log(anotherstring);
 
 const newstringone = "     hitesh       "
 
 console.log(newstringone);
+
 console.log(newstringone.trim()); //it will remove the spaces
 
+//console.log(newstringone);
 const url = "https://hitesh.com.hitesh%20choudhary"
 console.log(url.replace('%20' , '-')); //replace : learn the methods learn about split and include method
 
+console.log(url.replace("hitesh.com" , "kashyap.com"));
+
+// const onestring = "kashyap"
+
+// console.log(onestring);
+
+// console.log(`my surname is ${onestring.toLocaleUpperCase()} and i want the whole world `);
+
+// console.log(onestring.charAt(2));
