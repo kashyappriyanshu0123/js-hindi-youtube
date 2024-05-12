1ce1dc3b362b1751831632ebabeb8c2e3eda758c
kashyappriyanshu0123 <93027668+kashyappriyanshu0123@users.noreply.github.com>
2024-05-12 09:28:42 +0000

    array

--- 02_basics/01_array.js
+++ 02_basics/01_array.js
@@ -4,7 +4,7 @@ const myheros = ["dad" , "grandfather","rounak bhaiya"]
 
 const myarr2 = new Array(1,2,3,4,5)
 //console.log(myheros[1]);
-
+console.log(myheros[2]);
 //Array Methods
 
 myarr.push(7); //new element add to the array
@@ -13,7 +13,7 @@ myarr.push(9);
 myarr.pop() //rmove the last element from the array
 
 myarr.shift(); //remove the element from the starting
-//console.log(myarr); 
+console.log(myarr); 
 
 // console.log(myarr.includes(9));
 // console.log(myarr.indexOf(4));
@@ -21,7 +21,7 @@ myarr.shift(); //remove the element from the starting
 const newarr = myarr.join() //join will convert the array into string (go through this topic)
 
 // console.log(myarr);
-// console.log(typeof newarr);
+console.log(typeof newarr);
  
 
 //slice ,splice
@@ -31,7 +31,7 @@ console.log("A ", myarr);
 const myn1 = myarr.slice(1, 3)
 
 console.log(myn1); 
-console.log("B ", myarr); //after using splice
+console.log("B ", myarr); //after using slice
 
 
 const myn2 = myarr.splice(1, 3)
