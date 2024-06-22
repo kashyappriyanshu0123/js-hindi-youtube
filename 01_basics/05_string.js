7412cfe92e439e56b8aa1fa27988bee9b924cb12
kashyappriyanshu0123 <93027668+kashyappriyanshu0123@users.noreply.github.com>
2024-06-22 13:38:10 +0000

    string

--- 01_basics/05_string.js
+++ 01_basics/05_string.js
@@ -5,10 +5,10 @@ console.log(name + repcount);
 console.log(`hello my name is ${name.toUpperCase()} and my repo count is ${repcount}`); //we can also add method here as we added toupper
 
 console.log(`hey my name is ${name.toLowerCase()} and my reocount is : ${repcount}`);
-const gamename = new String("hitesh-hc")
+const gamename = new String("hitesh-hc") 
 
 console.log(gamename[0]); //for accessing the 0th key
-console.log(gamename.__proto__); 
+console.log(gamename.__proto__); // => {}
 
 console.log(gamename.charAt(2));
 console.log(gamename.indexOf('i'));
@@ -33,16 +33,20 @@ console.log(newstringone.trim()); //it will remove the spaces
 const url = "https://hitesh.com.hitesh%20choudhary"
 console.log(url.replace('%20' , '-')); //replace : learn the methods learn about split and include method
 
+console.log(url.includes('hitesh'));
+console.log(gamename.split('-'));
 //+++++My practise
 
-console.log(url.replace("hitesh.com" , "kashyap.com"));
+// console.log(url.replace("hitesh.com" , "kashyap.com"));
 
-const onestring = "kashyap"
+// const onestring = "kashyap"
 
-console.log(onestring);
+// console.log(onestring);
 
-console.log(`my surname is ${onestring.toLocaleUpperCase()} and i want the whole world `);
+// console.log(`my surname is ${onestring.toLocaleUpperCase()} and i want the whole world `);
 
-console.log(onestring.charAt(2));
+// console.log(onestring.charAt(2));
 
-console.log(onestring.indexOf('k'));
+// console.log(onestring.indexOf('k'));
+
+console.log(`my surname is  kashyap and i am learning from ${gamename}`);
