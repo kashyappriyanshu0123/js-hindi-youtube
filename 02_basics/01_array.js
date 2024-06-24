c3b6be1824d463de0987a1208bc9b24b23ebd9b3
kashyappriyanshu0123 <93027668+kashyappriyanshu0123@users.noreply.github.com>
2024-06-24 08:09:39 +0000

    array

--- 02_basics/01_array.js
+++ 02_basics/01_array.js
@@ -4,16 +4,27 @@ const myheros = ["dad" , "grandfather","rounak bhaiya"]
 
 const myarr2 = new Array(1,2,3,4,5)
 //console.log(myheros[1]);
-console.log(myheros[2]);
+// console.log(myheros[2]);
+
+
 //Array Methods
 
-myarr.push(7); //new element add to the array
-myarr.push(9);
 
-myarr.pop() //rmove the last element from the array
+// myarr.push(7); //new element add to the array
+// myarr.push(9);
+// console.log(myarr);
 
-myarr.shift(); //remove the element from the starting
-console.log(myarr); 
+// myarr.pop() //rmove the last element from the array
+// console.log(myarr);
+
+// myarr.unshift(8) //inseert the element to the begining
+// console.log(myarr); 
+
+// myarr.shift(); //remove the element from the starting
+// console.log(myarr);
+
+// myarr.shift()
+// console.log(myarr);
 
 // console.log(myarr.includes(9));
 // console.log(myarr.indexOf(4));
@@ -21,20 +32,21 @@ console.log(myarr);
 const newarr = myarr.join() //join will convert the array into string (go through this topic)
 
 // console.log(myarr);
-console.log(typeof newarr);
+// console.log(newarr);
+// console.log(typeof newarr);
  
 
 //slice ,splice
 
-console.log("A ", myarr);
+ console.log("A ", myarr);
 
-const myn1 = myarr.slice(1, 3)
+const myn1 = myarr.slice(0, 3)
 
-console.log(myn1); 
-console.log("B ", myarr); //after using slice
+console.log(myn1);  //the values after using slace method
+console.log(`B original ${myarr},it got changed`);     //the original array : B  [ 2, 3, 4, 5, 6 ]
 
 
 const myn2 = myarr.splice(1, 3)
 
-console.log("C ", myarr); // after using splice
-console.log(myn2); //from array the slice o/p get removed splice o/p :[4,5,6] after splice o/p : [3,7]
+console.log("C ", myarr); // the original array gets manipulated after using splice o/p : C  [ 2, 6 ]
+console.log(myn2); //from array the splice o/p get removed splice o/p :[4,5,6] after splice o/p : [3,7]
