d06f4d55a94382094c2997d87a5552762823bff8
kashyappriyanshu0123 <93027668+kashyappriyanshu0123@users.noreply.github.com>
2024-06-24 12:17:00 +0000

    object

--- 02_basics/04_object.js
+++ 02_basics/04_object.js
@@ -1,4 +1,4 @@
-//objects singleton
+//objects singleton or how can i declare object with the help of constructor
 
 //creating object 2 type
 const tinderuser = {} //non - singleton object
@@ -8,8 +8,11 @@ tinderuser.id = "123abc"
 tinderuser.name = "Sammy"
 tinderuser.isLoggedIn = false
 
-console.log(tinder);
-console.log(tinderuser);
+tinder.name = "kashyap"
+tinder.rollno = 123
+
+// console.log(tinder);
+// console.log(tinderuser);
 
 const regularuser = {
     email : "priyanshukashyap.com",
@@ -21,12 +24,13 @@ const regularuser = {
     }
 }
 
-console.log(regularuser.fullnames.userfullname.firstname); //for accessing objects in objects
+// console.log(regularuser.fullnames.userfullname.firstname); //for accessing objects in objects
 
 //for combining the objects
-const obj1 = {1: "a",
-2: "b"
-}
+const obj1 = {
+  1 : "a",
+  2 : "b"
+   }
 const obj2 = {
     3 : "c",
     4 : "d"
@@ -37,14 +41,16 @@ const obj4 = {
 }
 
 // const obj3 = Object.assign(obj1,obj2)
-//console.log(obj3); //{ '1': 'a', '2': 'b', '3': 'c', '4': 'd' }
+// console.log(obj3);                      //{ '1': 'a', '2': 'b', '3': 'c', '4': 'd' }
 
-//const obj3 = Object.assign({},obj1,obj2,obj4)
-//console.log(obj3);
+// const obj3 = Object.assign({},obj1,obj2,obj4)
+// console.log(obj3);
 
-//const obj3 = {obj1 , obj2}
-//console.log(obj3); //{ obj1: { '1': 'a', '2': 'b' }, obj2: { '3': 'c', '4': 'd' } } ->the object is binded inside the object
+// const obj3 = {obj1 , obj2}
+// console.log(obj3);              //   { obj1: { '1': 'a', '2': 'b' }, obj2: { '3': 'c', '4': 'd' } } ->the object is binded inside the object
 
+// const obj3 = Object.assign(obj1,obj4)
+// console.log(obj3);
 
 //const obj3 ={...obj1, ...obj2} //spread operator
 //console.log(obj3);
@@ -63,14 +69,15 @@ const users = [ //the value comes from databse
     email : 'ppeoei@gmail.com'
   }
 ]
-console.log(users[1].email); 
+console.log(users[1].email);  // =>  [] we are using brackets because we binded the object in the array 
+console.log(users[2].id);
 
 //console.log(tinderuser);
 //  console.log(Object.keys(tinderuser));
 //  console.log(Object.values(tinderuser));
 //  console.log(Object.entries(tinderuser));
 
-//  console.log(tinderuser.hasOwnProperty('isloggedin'));
+ console.log(tinderuser.hasOwnProperty('isloggedin'));
 
 
  //+++++++ objects destructuring and json api
