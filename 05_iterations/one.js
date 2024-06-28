b0e9f098534a1d318957b1414e7147a5820d0cd6
kashyappriyanshu0123 <93027668+kashyappriyanshu0123@users.noreply.github.com>
2024-06-28 13:00:20 +0000

    one

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -1,52 +1,70 @@
 //for
 
-for (let i = 0; i < 10; i++) {
-    const element = i
-    if(element == 5)
-    {
-        //console.log("5 is best number");
-    }
-    //console.log(element);
-}
+// for (let i = 0; i < 10; i++) {
+//     const element = i
+//     if(element == 5)
+//     {
+//         console.log("5 is best number");
+//     }
+//     console.log(element);
+// }
+// 
 
-for (let i = 1; i < 10; i++) {
-    console.log(`Outer loop value : ${i}`);
-    for (let j = 1; j < 10; j++) {
-      //  console.log(`inner loop value ${j} and inner loop ${i} `);
-        console.log(i + ' * ' + j + ' = '  +i*j);
+
+// for (let i = 1; i < 10; i++) {
+//     console.log(`Outer loop value : ${i}`);
+//     for (let j = 1; j <= 10; j++) {
+//        // console.log(`inner loop value ${j} and inner loop ${i} `);
+//       //  console.log(i + ' * ' + j + ' = '  +i*j);
+
+//       console.log(i + "*" + j + " = " + i*j );
         
-    }
+//     }
 
     
-}
+//  }
 
 
-let myarray = ["flash" , "superman" , "batman"]
-for (let index = 0; index < myarray.length; index++) {
-    const element = myarray[index];
-    console.log(element);
-}
+// let myarray = ["flash" , "superman" , "batman"]
+// for (let index = 0; index < myarray.length; index++) {
+//     const element = myarray[index];
+//     console.log(element);
+// }
 
-//break and continue
+
+
+// //break and continue
+
+
+// // for (let index = 0; index < 20 ; index++) {
+// //      if(index == 5)
+// //      {
+// //         console.log("5 detected");
+// //         break;
+// //      }
+// //      console.log(`value of i ${index}`);
+    
+// // }
+
+
 
 // for (let index = 0; index < 20 ; index++) {
 //      if(index == 5)
 //      {
-//         console.log("5 detected");
-//         break;
+//         console.log("5 detected");  // 5 will not get printed
+//         continue
 //      }
 //      console.log(`value of i ${index}`);
     
 // }
 
-
-
-for (let index = 0; index < 20 ; index++) {
-     if(index == 5)
-     {
-        console.log("5 detected");  // 5 will not get printed
-        continue
-     }
-     console.log(`value of i ${index}`);
+for (let index = 0; index < 10; index++) {
+    const element = index;
+    if(element == 5)
+        {
+            console.log(`come out of thee loop ${element} detected`);
+            break;
+        }
+        console.log(element);
     
 }
