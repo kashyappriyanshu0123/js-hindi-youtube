0796521418204e595f8b365d157bb4cca63b1b17
kashyappriyanshu0123 <93027668+kashyappriyanshu0123@users.noreply.github.com>
2024-06-29 13:18:29 +0000

    five

--- 05_iterations/five.js
+++ 05_iterations/five.js
@@ -1,25 +1,28 @@
 const coding = ["js" , "ruby" , "python" ,'cpp' , "java"]
 
 //for - each loop : it's a higher order function
-//callback functions dosen;t have name 
+//callback functions dosen't have name 
 
-// coding.forEach(function (val){
+
+
+// coding.forEach(function (val){         // val will bring each element of array as a parameter, then i can manipulate the value in the body of the function
 //   console.log(val);
 // } )
 
-// coding.forEach(  (val) =>{
-//     console.log(val);
+// coding.forEach( (items) => {
+//     console.log(items);
 // })
+
 
 // function printMe(item){
 //     console.log(item);
 // }
 
-// coding.forEach(printMe)
+// coding.forEach(printMe) //here i have to just give reference the printing will be done without execution means i don't have to give this bracket()
 
-// coding.forEach((val,index,arr) =>{
-// console.log(val,index,arr);
-// } )
+// coding.forEach((item,ind,arr)=>{
+//   console.log(item,ind,arr);
+// })
 
 const myCoding = [ //objects inside the array
     {
@@ -35,6 +38,6 @@ const myCoding = [ //objects inside the array
         languagefilename : "py"
     },
 ]
-myCoding.forEach((item) => {
-    console.log(item.languagefilename);  // js java py
-})
+// myCoding.forEach((item) => {
+//     console.log(item.languagefilename);  // js java py
+// })
