1c60bce0219bea2d93b31108db28a522bc04b6fc
kashyappriyanshu0123 <93027668+kashyappriyanshu0123@users.noreply.github.com>
2024-06-29 13:27:52 +0000

    seven

--- 05_iterations/seven.js
+++ 05_iterations/seven.js
@@ -7,6 +7,6 @@ const mynums = [1,2,3,4,5,6,7,8,9,10]
 
 const newnums = mynums
                  .map( (num) => num * 10)
-                 .map( (val) => val + 1 )
-                 .filter((item) => item > 40)  
+                 .map((num) => num +15 )
+                 .filter((num) => num>20)
 console.log(newnums);
