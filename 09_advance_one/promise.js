36a1aa426274003fbf10a1326d5c3e115dbae0cd
kashyappriyanshu0123 <93027668+kashyappriyanshu0123@users.noreply.github.com>
2024-07-06 09:40:42 +0000

    promise

--- 09_advance_one/promise.js
+++ 09_advance_one/promise.js
@@ -28,65 +28,63 @@
 //     console.log("async 2 resolved");
 // })
 
-// const promisethree = new Promise(function(resolve,reject){
-// setTimeout(function(){
-//   resolve({username : "Priyanshu kumar" , email : "kashyap@123.com" })
-//   },1000)
-// })
-
-
-
-// promisethree.then(function(user){
-//     console.log(user);
-// })
-
-
-const promiseFour = new Promise(function(resolve, reject){
-    setTimeout(function(){
-        let error = false
-        if (!error) {
-            resolve({username: "hitesh", password: "123"})
-        } else {
-            reject('ERROR: Something went wrong')
-        }
-    }, 1000)
+const promisethree = new Promise(function(resolve,reject){
+setTimeout(function(){
+  resolve({username : "Priyanshu kumar" , email : "kashyap@123.com" })  //the values which are passing in the .then callback function is because of resolve , so in connclusion i get that whatever i passed in the parameter of resolve we get the value in .then
+  },1000)
 })
 
- promiseFour
- .then((user) => {
+promisethree.then(function(user){  //     the reolve is connected with .then i can expect that by defult some data will get passed in the function let's suppose i'm calling that object as a user
     console.log(user);
-    return user.username
-}).then((username) => {
-    console.log(username);
-}).catch(function(error){
-    console.log(error);
-})
-.finally(() => {
-    console.log("the promise is either resolved or rejected"); //it will get printed if the prmise is resolve or rejected
 })
 
-const promisefive = new Promise(function(resolve,reject){
-    setTimeout(function(){
-       let error = true
-       if(!error){
-        resolve({username: "javascript" , password : "123"})
-       }
-       else{
-        reject("errror : js went wrong")
-       }
-    },1000)
-})
 
-async function consumefive(){
-try {
-    const response = await promisefive
-    console.log(response);
+// const promiseFour = new Promise(function(resolve, reject){
+//     setTimeout(function(){
+//         let error = false
+//         if (!error) {
+//             resolve({username: "hitesh", password: "123"})
+//         } else {
+//             reject('ERROR: Something went wrong')
+//         }
+//     }, 1000)
+// })
+
+//  promiseFour
+//  .then((user) => {
+//     console.log(user);
+//     return user.username
+// }).then((username) => {
+//     console.log(username);
+// }).catch(function(error){
+//     console.log(error);
+// })
+// .finally(() => {
+//     console.log("the promise is either resolved or rejected"); //it will get printed if the prmise is resolve or rejected
+// })
+
+// const promisefive = new Promise(function(resolve,reject){
+//     setTimeout(function(){
+//        let error = true
+//        if(!error){
+//         resolve({username: "javascript" , password : "123"})
+//        }
+//        else{
+//         reject("errror : js went wrong")
+//        }
+//     },1000)
+// })
+
+// async function consumefive(){
+// try {
+//     const response = await promisefive
+//     console.log(response);
     
-} catch (error) {
-    console.log(error);
-}
-} 
-consumefive()
+// } catch (error) {
+//     console.log(error);
+// }
+// } 
+// consumefive()
 
 //both are same async await and .then , (.catch)
 
@@ -101,14 +99,14 @@ consumefive()
 // }
 // getallusers()
 
-fetch('https://api.github.com/users/hiteshchoudhary')
-.then((response) => {
-    return response.json()
-})
-.then((data) => {
-    console.log(data); //response.json return will handle by this (.then)
-})
-.catch((error) => console.log(error))
+// fetch('https://api.github.com/users/hiteshchoudhary')
+// .then((response) => {
+//     return response.json()
+// })
+// .then((data) => {
+//     console.log(data); //response.json return will handle by this (.then)
+// })
+// .catch((error) => console.log(error))
 
 // promise.all
 // yes this is also available, kuch reading aap b kro.
