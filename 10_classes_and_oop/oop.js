1eb062e9e7ef4300d0d4cda894969945865c2cee
kashyappriyanshu0123 <93027668+kashyappriyanshu0123@users.noreply.github.com>
2024-07-06 10:58:11 +0000

    oop

--- 10_classes_and_oop/oop.js
+++ 10_classes_and_oop/oop.js
@@ -1,16 +1,18 @@
-const user = {
-    username : "priyanshu",
-    logincount : 8,
-    signedin : true,
 
-    getuserdetails : function(){ //getuserdetails is a function
-      //  console.log("got user details from database");
-    //   console.log(`username: ${this.username}`); //for accessing usernamee we have to use this keyword
-    //   console.log(this);
-    }
 
-}
-// console.log(user);
+// const user = {
+//     username : "priyanshu",
+//     email : "priyanshukashyap024GMAIL.COM",
+//     logincount : 5,
+
+//     getuserdetails : function(){
+//         console.log(`Username : ${this.username}`);
+//         console.log(this);
+//     }
+
+// }
+// console.log(user.username);
+// console.log(user["logincount"]);
 // console.log(user.getuserdetails());
 
 function User(username, loginCount, isLoggedIn){
@@ -27,13 +29,16 @@ function User(username, loginCount, isLoggedIn){
 }
 
 // const userOne =  User("hitesh", 12, true)
-// const userTwo =  User("ChaiAurCode", 11, false) // this will rewrite the value of userone even the functon two is not called
+// const userTwo =  User("ChaiAurCode", 11, false) // this will rewrite the value of userone even the functon two is not 
 
+
+const user1 = new User("kashyap",5,true)
+user1.greeting()
 
 const userOne = new User("hitesh", 12, true)
 const userTwo = new User("ChaiAurCode", 11, false) 
 
-console.log(userOne);
-console.log(userTwo);
-console.log(userOne.constructor); // constructor property is reference about itself (o/p : [Function: User])
+// console.log(userOne);
+// console.log(userTwo);
+// console.log(userOne.constructor); // constructor property is reference about itself (o/p : [Function: User])
 //console.log(userTwo);
