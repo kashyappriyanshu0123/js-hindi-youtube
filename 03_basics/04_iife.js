9f4c84ec3fc45e3163423a5703787897ee687f96
kashyappriyanshu0123 <93027668+kashyappriyanshu0123@users.noreply.github.com>
2025-05-30 12:39:00 +0000

    iife

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,13 @@
+//Immediately invoked function expression(iife)
+
+//Named iife
+(function chai(){
+    console.log('DB connected');
+    
+} )(); //semicolon is important to stop this iife if i want to use another iife i have to apply the semicolon after the function execution
+
+//unnamed iife
+( (name) => {
+    console.log(`DB connected two ${name}`);
+    
+})(`kashyap`)
