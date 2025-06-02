c3dc6277ea0b77210fcc4bcda417713fbd1ea4f0
kashyappriyanshu0123 <93027668+kashyappriyanshu0123@users.noreply.github.com>
2025-06-02 12:34:48 +0000

    truthy

--- 04_controlflow/truthy.js
+++ 04_controlflow/truthy.js
@@ -13,22 +13,20 @@ if (userEmail) {
 //truthy values
 // "0" -> it's truthy bcoz it's in string , 'false' -> it's truthy bcoz it's in string , " " -> its truthy bcoz b/w empty string there is space in between , [], {}, function(){}
 
-if(userEmail.length === 0)
-{
+if (userEmail.length === 0) {
     console.log("array is empty");
 }
 
 const emptyobj = {}
 
 
-if(Object.keys(emptyobj).length === 0)
-{
+if (Object.keys(emptyobj).length === 0) {
     console.log("object is empty");
 }
 
-// Nullish Coalescing operator (??) :null defined
+// Nullish Coalescing operator (??) :null undefined
 
-let val1 
+let val1
 //val1 = 5 ?? 10 //5
 
 //val1 = 40 ?? 10 // 40 
