2f81d32e0d699c8deaa8977d377a8fe029ac4b4b
kashyappriyanshu0123 <93027668+kashyappriyanshu0123@users.noreply.github.com>
2025-06-17 09:36:55 +0000

    iife

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -7,7 +7,7 @@
 } )(); //semicolon is important to stop this iife if i want to use another iife i have to apply the semicolon after the function execution
 
 //unnamed iife
-( (name) => {
+( (name) => { // passing name in the parameters
     console.log(`DB connected two ${name}`);
     
 })(`kashyap`)
