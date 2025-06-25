fdfb7c74bf6705a09e5483f7b30d9e88b109ce29
kashyappriyanshu0123 <93027668+kashyappriyanshu0123@users.noreply.github.com>
2025-06-25 11:32:24 +0000

    one

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -1,5 +1,14 @@
 //for
 
+
+// for (let ind = 0; ind < 10; ind++) {
+//     const element = ind;
+//     console.log(element);
+
+
+// }
+
+
 // for (let i = 0; i < 10; i++) {
 //     const element = i
 //     if(element == 5)
@@ -14,37 +23,41 @@
 // for (let i = 1; i < 10; i++) {
 //     console.log(`Outer loop value : ${i}`);
 //     for (let j = 1; j <= 10; j++) {
-//        // console.log(`inner loop value ${j} and inner loop ${i} `);
-//       //  console.log(i + ' * ' + j + ' = '  +i*j);
+//         // console.log(`inner loop value ${j} and outer loop ${i} `);
 
-//       console.log(i + "*" + j + " = " + i*j );
-        
+//      //   console.log(i + ' * ' + j + ' = ' + i * j);
+
+//         // console.log(i, ' * ', j + ' = ', i * j);
+
+//         // console.log(i + "*" + j + " = " + i*j );
+
 //     }
 
-    
-//  }
+
+// }
 
 
-// let myarray = ["flash" , "superman" , "batman"]
+// let myarray = ["flash", "superman", "batman"]
+// console.log(myarray.length);
+
 // for (let index = 0; index < myarray.length; index++) {
-//     const element = myarray[index];
+//     const element = myarray[index]; //to access array we have to give the index value here it's index
 //     console.log(element);
 // }
 
 
 
-// //break and continue
+// break and continue
 
 
-// // for (let index = 0; index < 20 ; index++) {
-// //      if(index == 5)
-// //      {
-// //         console.log("5 detected");
-// //         break;
-// //      }
-// //      console.log(`value of i ${index}`);
-    
-// // }
+for (let index = 0; index < 20; index++) {
+    if (index == 5) {
+        console.log("5 detected");
+        break;
+    }
+    console.log(`value of i ${index}`);
+
+}
 
 
 
@@ -55,16 +68,15 @@
 //         continue
 //      }
 //      console.log(`value of i ${index}`);
-    
+
 // }
 
 for (let index = 0; index < 10; index++) {
     const element = index;
-    if(element == 5)
-        {
-            console.log(`come out of thee loop ${element} detected`);
-            break;
-        }
-        console.log(element);
-    
+    if (element == 5) {
+        console.log(`come out of thee loop ${element} detected`);
+        break;
+    }
+    console.log(element);
+
 }
