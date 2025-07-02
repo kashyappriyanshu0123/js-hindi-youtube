e53cfd30daf13f1e5aa261b51132a11c598ea307
kashyappriyanshu0123 <93027668+kashyappriyanshu0123@users.noreply.github.com>
2025-07-02 15:19:11 +0000

    eight

--- 05_iterations/eight.js
+++ 05_iterations/eight.js
@@ -5,15 +5,15 @@ const mynums = [1, 2, 3]
 // const mytotal = mynums.reduce( function (acc , currval) {
 //    console.log(`acc : ${acc} and currval ${currval}`);     //it's for better readability
 //   return acc + currval
-     
+
 // } ,0) //initial val = 0
-  
- 
+
+
 //both are same
 
- 
-const mytotal = mynums.reduce( (acc,curr) => acc+curr , 0) //initial value 0
- 
+
+const mytotal = mynums.reduce((acc, curr) => acc + curr, 0) //initial value 0
+
 //console.log(mytotal);
 
 
@@ -22,25 +22,25 @@ const mytotal = mynums.reduce( (acc,curr) => acc+curr , 0) //initial value 0
 
 const shoppingcart = [
     {
-        itemname : 'jscourse',
-        price : 2599
+        itemname: 'jscourse',
+        price: 2599
     },
     {
-        itemname : 'pythoncourse',
-        price : 6599
+        itemname: 'pythoncourse',
+        price: 6599
     },
     {
-        itemname : 'cppcourse',
-        price : 4599
+        itemname: 'cppcourse',
+        price: 4599
     },
     {
-        itemname : 'webdevcourse',
-        price : 1599
+        itemname: 'webdevcourse',
+        price: 1599
     },
     {
-        itemname : 'datasciencecourse',
-        price : 34567
+        itemname: 'datasciencecourse',
+        price: 34567
     },
 ]
-const shopadd = shoppingcart.reduce( (acc, val) => acc + val.price,0 )
-console.log(shopadd);
+const shopadd = shoppingcart.reduce((acc, val) => acc + val.price, 0) // here i have to intoduce a functionality that every prices that are in the shopping cart i have to add it here
+console.log(shopadd); // here it's simple but in app we have to verify that in accumulator we are getting a number or string we have to verify that
