25137fdc97ccada4872eda0ebe6a37954f1e61e1
kashyappriyanshu0123 <93027668+kashyappriyanshu0123@users.noreply.github.com>
2025-08-15 21:22:07 +0000

    promise

--- 09_advance_one/promise.js
+++ 09_advance_one/promise.js
@@ -5,13 +5,16 @@
 //     setTimeout(function(){
 //         myresolve()//for connecting myresolve and (.then) we have to call resolve method 
 //         console.log('Async task is compelete'); //it will execute first
-       
+
 //     }, 1000)
 // })
+
 // //"Consuming code"
 // promiseOne.then(function(){ //(.then direct connection with myresolve)
-//     console.log("Promise consumed");
+//     console.log("Promise consumed");  //with .then i get a function with it and this function automatically receives an argumnet and the argument work is to return the value which we get by settimeout 
 // })
+
+//2nd method to create promise
 
 // new Promise( (resolve,reject) =>
 // {
@@ -24,44 +27,50 @@
 //     //     resolve()
 //     // })
 // })
+
+// //bcoz we didn't store the func in var so i can directly call .then
+
 // .then(function(){ //.then direct connection wth resolve
 //     console.log("async 2 resolved");
 // })
 
-const promisethree = new Promise(function(resolve,reject){
-setTimeout(function(){
-  resolve({username : "Priyanshu kumar" , email : "kashyap@123.com" })  //the values which are passing in the .then callback function is because of resolve , so in connclusion i get that whatever i passed in the parameter of resolve we get the value in .then
-  },1000)
-})
-
-promisethree.then(function(user){  //     the reolve is connected with .then i can expect that by defult some data will get passed in the function let's suppose i'm calling that object as a user
-    console.log(user);
-})
-
-
-// const promiseFour = new Promise(function(resolve, reject){
-//     setTimeout(function(){
-//         let error = false
-//         if (!error) {
-//             resolve({username: "hitesh", password: "123"})
-//         } else {
-//             reject('ERROR: Something went wrong')
-//         }
-//     }, 1000)
+// const promisethree = new Promise(function(resolve,reject){
+// setTimeout(function(){
+//   resolve({username : "Priyanshu kumar" , email : "kashyap@123.com" })  //the values which are passing in the .then callback function is because of resolve , so in connclusion i get that whatever i passed in the parameter of resolve we get the value in .then
+//   },1000)
 // })
 
-//  promiseFour
-//  .then((user) => {
+// promisethree.then(function(user){  //     the resolve is connected with .then i can expect that by defult some data will get passed in the function let's suppose i'm calling that whole object as a user
 //     console.log(user);
-//     return user.username
-// }).then((username) => {
-//     console.log(username);
-// }).catch(function(error){
-//     console.log(error);
-// })
-// .finally(() => {
-//     console.log("the promise is either resolved or rejected"); //it will get printed if the prmise is resolve or rejected
-// })
+//})
+
+
+
+const promiseFour = new Promise(function (resolve, reject) {
+  setTimeout(function () {
+    let error = true // when i make this false it will show error
+    if (!error) {
+      resolve({ username: "priyanshu", password: "123" })
+    } else {
+      reject('ERROR: Something went wrong')
+    }
+  }, 1000)
+})
+
+promiseFour
+  .then((user) => {
+    console.log(user);
+    return user.username
+  })
+  .then((username) => {
+    console.log(username);
+  })
+  .catch(function (error) {
+    console.log(error);
+  })
+  .finally(() => {
+    console.log("the promise is either resolved or rejected"); //it will get printed if the prmise is resolve or rejected
+  })
 
 // const promisefive = new Promise(function(resolve,reject){
 //     setTimeout(function(){
@@ -79,7 +88,7 @@ promisethree.then(function(user){  //     the reolve is connected with .then i c
 // try {
 //     const response = await promisefive
 //     console.log(response);
-    
+
 // } catch (error) {
 //     console.log(error);
 // }
@@ -88,16 +97,16 @@ promisethree.then(function(user){  //     the reolve is connected with .then i c
 
 //both are same async await and .then , (.catch)
 
-// async function getallusers(){
-//     try {
-//         const response = await fetch ('https://jsonplaceholder.typicode.com/users')
-//         const data = await response.json()
-//         console.log(data);
-//     } catch (error) {
-//         console.log("E:",error);
-//     }
-// }
-// getallusers()
+async function getallusers() {
+  try {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users')
+    const data = await response.json()
+    console.log(data);
+  } catch (error) {
+    console.log("E:", error);
+  }
+}
+getallusers()
 
 // fetch('https://api.github.com/users/hiteshchoudhary')
 // .then((response) => {
@@ -110,3 +119,5 @@ promisethree.then(function(user){  //     the reolve is connected with .then i c
 
 // promise.all
 // yes this is also available, kuch reading aap b kro.
+
+
