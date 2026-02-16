58ced0a8e3c8d303034629eee00ae77d820fcad8
kashyappriyanshu0123 <93027668+kashyappriyanshu0123@users.noreply.github.com>
2026-02-16 12:13:20 +0000

    one

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -37,6 +37,7 @@
 // }
 
 
+
 // let myarray = ["flash", "superman", "batman"]
 // console.log(myarray.length);
 
@@ -50,14 +51,14 @@
 // break and continue
 
 
-for (let index = 0; index < 20; index++) {
-    if (index == 5) {
-        console.log("5 detected");
-        break;
-    }
-    console.log(`value of i ${index}`);
+// for (let index = 0; index < 20; index++) {
+//     if (index == 5) {
+//         console.log("5 detected");
+//         break;
+//     }
+//     console.log(`value of i ${index}`);
 
-}
+// }
 
 
 
@@ -71,12 +72,41 @@ for (let index = 0; index < 20; index++) {
 
 // }
 
-for (let index = 0; index < 10; index++) {
-    const element = index;
-    if (element == 5) {
-        console.log(`come out of thee loop ${element} detected`);
-        break;
-    }
-    console.log(element);
+// for (let index = 0; index < 10; index++) {
+//     const element = index;
+//     if (element == 5) {
+//         console.log(`come out of thee loop ${element} detected`);
+//         break;
+//     }
+//     console.log(element);
 
+// }
+
+for(let i=0; i<10; i++){
+    for(let j=0; j<10; j++)
+    {
+        if(j==7)
+        {
+            break;
+        }
+        else
+        {
+            console.log(`value of i is: ${i} and j is ${j}`);
+            
+        }
+    }
+}
+
+// break example for double loop
+for (let i = 0; i < 3; i++) {
+    console.log("Outer loop i =", i);
+
+    for (let j = 0; j < 5; j++) {
+        if (j === 2) {
+            break; // stops inner loop only
+        }
+        console.log("   Inner loop j =", j);
+    }
+
+    console.log("Outer loop continues...");
 }
