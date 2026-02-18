c2ca4f8e8676b8bef3a1c605a328eec01b6a29a3
kashyappriyanshu0123 <93027668+kashyappriyanshu0123@users.noreply.github.com>
2026-02-18 08:45:58 +0000

    eight

--- 05_iterations/eight.js
+++ 05_iterations/eight.js
@@ -8,11 +8,18 @@ const mynums = [1, 2, 3]
 
 // } ,0) //initial val = 0
 
+const total = mynums.reduce(function(acc,currval)
+{
+    console.log(`acc : ${acc} and currvalue : ${currval}`);
+    
+    return acc+currval
+},10)
+console.log(total);
 
 //both are same
 
 
-const mytotal = mynums.reduce((acc, curr) => acc + curr, 0) //initial value 0
+//const mytotal = mynums.reduce((acc, curr) => acc + curr, 0) //initial value 0
 
 //console.log(mytotal);
 
@@ -42,5 +49,7 @@ const shoppingcart = [
         price: 34567
     },
 ]
-const shopadd = shoppingcart.reduce((acc, val) => acc + val.price, 0) // here i have to intoduce a functionality that every prices that are in the shopping cart i have to add it here
+const shopadd = shoppingcart.reduce((acc, val) => acc + val.price,0) // here i have to intoduce a functionality that every prices that are in the shopping cart i have to add it here
 console.log(shopadd); // here it's simple but in app we have to verify that in accumulator we are getting a number or string we have to verify that
+
+// acc is used for intial value that's 0 and val is used to trac down the current value starts from 2599
